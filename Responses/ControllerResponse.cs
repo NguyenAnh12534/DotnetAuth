9bff186a2a020c3de1f062c3d735125129691c3d
unknown <just4codee@gmail.com>
2023-09-21 22:46:53 +0700

    Initialize repo

---
+++ Responses/ControllerResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthAPI.Responses
+{
+    public class ControllerResponse<T>
+    {
+        public T? Data { get; set; } = default;
+
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; } = string.Empty;
+
+        public IDictionary<string, string[]> Errors { get; set; } = default!;
+    }
+}
